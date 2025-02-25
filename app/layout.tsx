8e6f48e560e277a4a72a993492e378412fd4d2ea
Shrajal Kaurav <96686794+Shrajalkaurav@users.noreply.github.com>
2025-02-25 23:36:44 +0530

    Add new files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
+import { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Cloudplay-Drive",
+};
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body>{children}</body>
+      <body className={`${poppins.variable} font-poppins antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <div>Cloudplay_Drive</div>;
-}
